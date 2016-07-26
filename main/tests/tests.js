75b329971bed4302a9a43577a950b234f8a579b1
ArthurFaugerasCI <arthur@cambridge-intelligence.com>
2016-07-26 10:57:33 +0100

    Adding errors

--- main/tests/tests.js
+++ main/tests/tests.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 describe("Test suite", function() {
     it("This function must return true", function() {
         expect(returnTrue()).toBe(true);
@@ -6,9 +7,9 @@ describe("Test suite", function() {
         expect(returnFalse()).toBe(false);
     });
     it("This tool must be available", function() {
-        expect(isToolAvailable()).toBe(true);
+        expect(isToolAvailable()).toBe(false);
     });
-    it("This function must return false", function() {
-        expect(notCovered()).toBe(false);
-    });
+    // it("This function must return false", function() {
+    //     expect(notCovered()).toBe(false);
+    // });
 });
