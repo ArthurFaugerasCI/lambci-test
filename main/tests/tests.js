143e8785fec22ec262f7c62ac946eddd3ca168cf
ArthurFaugerasCI <arthur@cambridge-intelligence.com>
2016-08-10 14:18:38 +0100

    Removing errors

--- main/tests/tests.js
+++ main/tests/tests.js
@@ -7,7 +7,7 @@ describe("Test suite", function() {
         expect(returnFalse()).toBe(false);
     });
     it("This tool must be available", function() {
-        expect(isToolAvailable()).toBe(false);
+        expect(isToolAvailable()).toBe(true);
     });
     it("This function must return false", function() {
         expect(notCovered()).toBe(false);
